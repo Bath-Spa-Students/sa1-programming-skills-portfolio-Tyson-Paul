8ae18fabd015e5d0f6625e54339e1bfe5908ae68
Tyson-Paul <tysonpaul0527@gmail.com>
2024-11-21 00:08:07 +0400

    Create Functions-Favorite Book.py

---
+++ z Bonus Exercises/Functions-Favorite Book.py
@@ -0,0 +1,6 @@
+#To define the function favorite_book with one parameter, title
+def favorite_book(title):
+    print(f"One of my favorite books is {title}.")
+
+#To call the function with a book title as an argument
+favorite_book("Alice in Wonderland")
