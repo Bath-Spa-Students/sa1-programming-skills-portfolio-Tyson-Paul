a2695ff4267d762efaf62e737b2d54db2278599e
Tyson-Paul <tysonpaul0527@gmail.com>
2024-11-21 00:07:33 +0400

    Create Control Structure Version 1.py

---
+++ z Bonus Exercises/Control Structures.py/Control Structure Version 1.py
@@ -0,0 +1,6 @@
+#Assigning the value 'green' to the alien_color variable
+alien_color = "green"
+
+#Check if the alien color is green
+if alien_color == "green":
+    print("You just earned 5 points!")
