f1072c671ed722ca91680c48f8910d492ae214ca
Tyson-Paul <tysonpaul0527@gmail.com>
2024-11-21 00:07:51 +0400

    Create Dictionaries.py

---
+++ z Bonus Exercises/Dictionaries.py
@@ -0,0 +1,13 @@
+#To store information about a person in a dictionary (first name, last name, age, city)
+person = {
+    "first_name": "Chris",
+    "last_name": "Martin",
+    "age": 18,
+    "city": "Dubai"
+}
+
+#To print each information stored in the dictionary
+print("First Name:", person["first_name"])
+print("Last Name:", person["last_name"])
+print("Age:", person["age"])
+print("City:", person["city"])
