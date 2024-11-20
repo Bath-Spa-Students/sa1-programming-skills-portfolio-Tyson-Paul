5641df16253de7cd2f0eed343e690aeeeeca91a1
Tyson-Paul <tysonpaul0527@gmail.com>
2024-11-21 00:04:18 +0400

    Create Variables (Favorite Number).py

---
+++ z Bonus Exercises/Variables (Favorite Number).py
@@ -0,0 +1,8 @@
+#Assigning your favorite number to a variable
+fav_number = 5
+
+#Create a message with your favorite number to reveal it
+message = f"My Favorite Number Is {fav_number}."
+
+#Print the message
+print(message)
