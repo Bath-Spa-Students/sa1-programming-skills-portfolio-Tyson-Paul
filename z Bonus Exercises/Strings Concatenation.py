1435cc43e90c2aa39899a8bc6eae27bd3f4337fa
Tyson-Paul <tysonpaul0527@gmail.com>
2024-11-21 00:03:56 +0400

    Create Strings Concatenation.py

---
+++ z Bonus Exercises/Strings Concatenation.py
@@ -0,0 +1,10 @@
+#Three strings in different variables
+string1 = "Tyson"
+string2 = "-"
+string3 = "Paul!"
+
+#Concatenate all strings together
+result = string1 + string2 + string3
+
+#Print the result
+print(result)
