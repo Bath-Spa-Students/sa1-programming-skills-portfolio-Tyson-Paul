b98470ee437b906f2ece1b6835889c19ed408cf4
Tyson-Paul <tysonpaul0527@gmail.com>
2024-11-21 12:05:14 +0400

    Delete Exercise 4 directory

--- Exercise 4/Primitive Quiz.py
+++
@@ -1,8 +0,0 @@
-# To ask the user a question 
-qns= input("What is the capital of France? ").strip().lower()
-
-# To check the answer if its true or not
-if qns == "paris":
-    print("The answer is Correct.")
-else:
-    print("The answer is Incorrect. The Correct answer is Paris.")
